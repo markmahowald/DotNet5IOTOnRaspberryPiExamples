773035be1529a194b3ef5fe85a4e948a69f50339
markmahowald <markmahowald0328@gmail.com>
2021-02-13 12:44:16 -0700

    Initial commit. SpinAMotor and CallSpinAMotor built

---
+++ CallSpinAMotor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace CallSpinAMotor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "/home/pi/SpinAMotor/SpinAMotor";
+            List<string> spinArguments = new List<string>(){ "1", "2", "-2" };
+
+            string argString = "";
+            foreach (var argument in spinArguments)
+            {
+                argString += argument + " ";
+            }
+
+            var processInfo = new ProcessStartInfo(arguments: argString, fileName: fileName) ;
+
+            Process proc = new Process();
+            proc.StartInfo = processInfo;
+            proc.Start();
+            Console.WriteLine("ran the hell out of that process");
+        }
+
+    }
+}
---
+++ SpinAMotor/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using Iot.Device.Uln2003;
+
+namespace SpinAMotor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pinout for Raspberry Pi 3
+            const int pin1 = 17;
+            const int pin2 = 27;
+            const int pin3 = 22;
+            const int pin4 = 10;
+            int degreesToRotate = 0;
+            bool parsable = Int32.TryParse(args[0], out degreesToRotate);
+
+            int[] parsedArgs = Array.ConvertAll(args, int.Parse);
+
+            using (Uln2003 motor = new Uln2003(pin1, pin2, pin3, pin4))
+            {
+
+                Console.WriteLine($"The number of args is {args.Length}.");
+
+                foreach (var parsedArg in parsedArgs)
+                {
+                    Console.WriteLine(parsedArg);
+                }
+                try
+                {
+
+                    motor.RPM = 15;
+                    motor.Mode = StepperMode.HalfStep;
+                    foreach (int rotationArg in parsedArgs)
+                    {
+                        motor.Step(rotationArg*4096);
+
+                        Thread.Sleep(1000*rotationArg);
+           
+                    }
+
+
+
+                    //int i = 0;
+                    //while (i<1)
+                    //{
+                    //    // Set the motor speed to 15 revolutions per minute.
+                    //    motor.RPM = 15;
+                    //    // Set the motor mode.  
+                    //    motor.Mode = StepperMode.HalfStep;
+                    //    // The motor rotate 2048 steps clockwise (180 degrees for HalfStep mode).
+                    //    motor.Step(2048);
+
+                    //    motor.Mode = StepperMode.FullStepDualPhase;
+                    //    motor.RPM = 8;
+                    //    // The motor rotate 2048 steps counterclockwise (360 degrees for FullStepDualPhase mode).
+                    //    motor.Step(-2048);
+
+                    //    motor.Mode = StepperMode.HalfStep;
+                    //    motor.RPM = 1;
+                    //    motor.Step(4096);
+                    //    i++;
+                    //}
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+                    //motor.Stop();
+                    //motor.Dispose();
+                }
+            }
+        }
+    }
+}
